9d3e037dbd680afc9c721b6c9bb78921ab26ea23
ryuzo.matsukawa <mitakado@msn.com>
2021-09-12 10:11:47 +0900

    lean jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 function App() {
-  return (
-    <div>
-        <h1>
-          Hello, world
-        </h1>
-    </div>
-  );
+  // return (
+  //   <h1>
+  //     Hello, world
+  //   </h1>
+  // );
+  const greeting = "Hi, React!";
+  let dom = <h1 className="foo">{greeting}</h1>;
+  dom = <input type="button" value="Click!" onClick={() => {console.log("I am clicked.")}} />;
+  return dom;
 }
 
 export default App;
