641d54f8705cc426f49243ddb0711b5767b7f167
ryuzo.matsukawa <mitakado@msn.com>
2021-09-12 10:21:37 +0900

    lean components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
+function Mio() {
+  return <div>Oha Mio-!</div>
+}
+
 function App() {
-  // return (
-  //   <h1>
-  //     Hello, world
-  //   </h1>
-  // );
-  const greeting = "Hi, React!";
-  let dom = <h1 className="foo">{greeting}</h1>;
-  dom = <input type="button" value="Click!" onClick={() => {console.log("I am clicked.")}} />;
-  return dom;
+  return (
+    <div>
+      <Mio />
+      <Mio />
+      <Mio />
+      <Mio />
+    </div>
+  );
 }
 
 export default App;
