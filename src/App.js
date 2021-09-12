8736c472cf38b12f7750779e53e1e3e74735e76f
ryuzo.matsukawa <mitakado@msn.com>
2021-09-12 13:03:31 +0900

    learn prop-types

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
+import PropTypes from 'prop-types';
+
+function createGenStr(gen) {
+  // console.log('start create generation string.');
+  if (gen !== undefined) {
+    // console.log('clear to check generation property.');
+    // console.log('generation number : ', gen);
+    if (gen === 1) {
+      return gen + "st";
+    } else if (gen === 2) {
+      return gen + "nd";
+    } else if (gen === 3) {
+      return gen + "rd";
+    } else {
+      return gen + "th";
+    }
+  }
+}
 
 function Members(props) {
-  return <div>Hi, I'm {props.name}. {props.group} group member.</div>
+  return <div>Hi, I'm {props.name}. {createGenStr(props.gen)} generation member.</div>
 }
 
 Members.defaultProps = {
-  group: "0th"
+  gen: 0
+}
+
+Members.propTypes = {
+  name: PropTypes.string.isRequired,
+  gen: PropTypes.number
 }
 
 class profile {
-  constructor(name, group) {
+  constructor(name, gen) {
     this.name = name;
-    this.group = group;
+    this.gen = gen;
   }
 }
 
 function App() {
   let profiles = [];
-  profiles.push(new profile("Fubuki", "1st"));
-  profiles.push(new profile("Ayame", "2nd"));
-  profiles.push(new profile("Polka", "5th"));
   profiles.push(new profile("Suisei"));
+  profiles.push(new profile("Fubuki", 1));
+  profiles.push(new profile("Ayame", 2));
+  profiles.push(new profile("Polka", 5));
 
   return (
     <div>
       {
         profiles.map((profile, index) => {
-          return <Members name={profile.name} group={profile.group} key={index} />
+          return <Members name={profile.name} gen={profile.gen} key={index} />
         })
       }
     </div>
