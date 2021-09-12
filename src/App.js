517c7b49d13cff30445607741d30ffd7375c4761
ryuzo.matsukawa <mitakado@msn.com>
2021-09-12 12:00:10 +0900

    lean props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 
-function Mio() {
-  return <div>Oha Mio-!</div>
+function Members(props) {
+  return <div>Hi, I'm {props.name}. {props.group} group member.</div>
+}
+
+Members.defaultProps = {
+  group: "0th"
+}
+
+class profile {
+  constructor(name, group) {
+    this.name = name;
+    this.group = group;
+  }
 }
 
 function App() {
+  let profiles = [];
+  profiles.push(new profile("Fubuki", "1st"));
+  profiles.push(new profile("Ayame", "2nd"));
+  profiles.push(new profile("Polka", "5th"));
+  profiles.push(new profile("Suisei"));
+
   return (
     <div>
-      <Mio />
-      <Mio />
-      <Mio />
-      <Mio />
+      {
+        profiles.map((profile, index) => {
+          return <Members name={profile.name} group={profile.group} key={index} />
+        })
+      }
     </div>
   );
 }
